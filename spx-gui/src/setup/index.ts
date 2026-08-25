84b45024b6a248c96d53f47301743931b81a2075
nighca <nighca@live.cn>
2026-08-25 15:52:55 +0800

    fix(spx-gui): disable automatic query refetching

--- spx-gui/src/setup/index.ts
+++ spx-gui/src/setup/index.ts
@@ -48,7 +48,17 @@ export function configureApp(app: VueApp, router: Router | undefined, config: Ap
   app.use(VueKonva as any, {
     customNodes: { CustomTransformer }
   })
-  app.use(VueQueryPlugin)
+  app.use(VueQueryPlugin, {
+    queryClientConfig: {
+      defaultOptions: {
+        queries: {
+          refetchOnMount: false,
+          refetchOnReconnect: false,
+          refetchOnWindowFocus: false
+        }
+      }
+    }
+  })
   app.use(createRadar())
   app.use(createSpotlight())
   app.use(createAppState())
